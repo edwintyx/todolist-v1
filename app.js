c151aeaf61b1be766909fe9fb5b81779539d5abb
Edwin <tan.edwin.yx@gmail.com>
2020-04-20 14:31:21 +0800

    added public directory for css + added styles.css

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const app = express();
 let items = ["Buy Food", "Cook Food"];
 
 app.set('view engine', 'ejs');
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
   let today = new Date();
