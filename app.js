c31c0d4beb3555345b493b96a07f4d9f279030c0
Edwin <tan.edwin.yx@gmail.com>
2020-04-20 13:07:14 +0800

    replace variables var to let as it is much safer

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food"];
+let items = ["Buy Food", "Cook Food"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', function(req, res) {
-  var today = new Date();
+  let today = new Date();
 
   const options = {
     weekday: 'long',
@@ -17,7 +17,7 @@ app.get('/', function(req, res) {
     day: 'numeric'
   }
 
-  var day = today.toLocaleDateString('en-US', options);
+  let day = today.toLocaleDateString('en-US', options);
 
   res.render("list", {
     kindOfDay: day,
@@ -25,7 +25,7 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
 
   res.redirect('/');
